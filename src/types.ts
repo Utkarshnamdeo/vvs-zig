e5239befabf3f224ead9a6f0ffe7be55d78a41aa
Utkarsh Namdeo <utkarshnamdeo@live.com>
2022-11-29 22:42:16 +0100

    faet: search comoponent and basic types

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export enum LocationType {
+  poi = 'poi',
+  stop = 'stop',
+  suburb = 'suburb',
+  location = 'location',
+}
+
+export interface Location {
+  id: string;
+  isGlobalId: boolean;
+  name: string;
+  disassembledName: string;
+  coord: Array<number>;
+  type: LocationType;
+  matchQuality: number;
+  isBest: boolean;
+  productClasses: Array<number>;
+  parent: {
+    id: string;
+    name: string;
+    type: string;
+  };
+  properties: { stopId: string };
+}
