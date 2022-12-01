49a3c77c0a03c7decd47c74edadb67c9d78ce907
Utkarsh Namdeo <utkarshnamdeo@live.com>
2022-12-01 20:12:10 +0100

    style: location search component styles fixes

--- src/components/SearchLocation/SearchLocation.tsx
+++ src/components/SearchLocation/SearchLocation.tsx
@@ -27,6 +27,7 @@ export const SearchLocation = ({
   loading,
   icon,
   className,
+  isDestination = false,
 }: {
   children: ReactNode;
   locations: Location[];
@@ -34,14 +35,22 @@ export const SearchLocation = ({
   loading: boolean;
   icon?: any;
   className?: string;
+  isDestination?: boolean;
 }) => {
   return (
-    <div className={clsx(className, 'my-1')}>
+    <div className={clsx(className, 'mt-6')}>
       <div className='relative -mx-3'>
         <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
           <img src={icon} alt='start' className='w-5 h-5' />
+
+          {/* trailing line from start -> to destination */}
+          {!isDestination && (
+            <div className='w-[1px] border-[0.5px] h-7 border-dashed border-[#979797] absolute top-5 left-[21px]'></div>
+          )}
         </div>
         {children}
+
+        {/* Location list after search */}
         {locations.length > 0 && (
           <ul className='absolute border-[1px] border-solid	border-slate-300 rounded bg-white p-3 z-10 max-h-60 overflow-scroll shadow-lg w-full top-5'>
             {locations.map((location) => (
@@ -61,6 +70,7 @@ export const SearchLocation = ({
                         alt=''
                       />
                     </div>
+
                     <div className='flex flex-col'>
                       <span className='text-sm font-medium text-gray-900 leading-3'>
                         {location.disassembledName || location.name}
@@ -76,6 +86,9 @@ export const SearchLocation = ({
           </ul>
         )}
       </div>
+
+      {/* input underline */}
+      <div className='mx-8 mt-2 border-solid border-b-2 border-slate-300' />
     </div>
   );
 };
@@ -90,7 +103,7 @@ export const SearchLocationInput = forwardRef<
       {...props}
       className={clsx(
         props.className,
-        'block w-full p-4 pl-10 text-sm text-gray-900 appearance-none bg-transparent mr-3 py-1 px-2 leading-tight focus:outline-none'
+        'block w-full pl-12 pr-8 text-sm text-gray-900 appearance-none bg-transparent leading-tight focus:outline-none'
       )}
     />
   );
